ab8320451d76200fd8fcea5a25350caf512b6301
jgalden <john.galden@optum.com>
2023-06-30 16:01:06 -0400

    Practice

---
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+
+        System.out.println("In controller");
+
+        return "Hello";
+
+    }
+
+}
